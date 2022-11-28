ecbfc2ac9269f5d16b91c7f6264dd4525d4055cb
mim-mel <mfblade2014@gmail.com>
2022-11-28 14:08:34 +0900

    design: Home페이지 ui수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Home = () => {
   const [localPosts, setLocalPosts] = useState([]);
@@ -10,16 +11,60 @@ const Home = () => {
   }, []);
 
   return (
-    <>
-      {localPosts.map(post => (
-        <div key={post.id}>
-          <Link style={{ display: 'block' }} to={`/detail/${post.id}`}>
-            {post.title}
-          </Link>
-        </div>
-      ))}
-    </>
+    <HomeWrap>
+      <PostWrap>
+        {localPosts.map(post => (
+          <PostBlock key={post.id}>
+            <PostLink style={{ display: 'block' }} to={`/detail/${post.id}`}>
+              <PostTitie>{post.title}</PostTitie>
+              <PostContent>{post.content}</PostContent>
+            </PostLink>
+          </PostBlock>
+        ))}
+      </PostWrap>
+    </HomeWrap>
   );
 };
+
+const HomeWrap = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 70px;
+`;
+
+const PostWrap = styled.div`
+  display: grid;
+  grid-template-columns: 32% 32% 32%;
+`;
+
+const PostBlock = styled.div`
+  width: 500px;
+  height: 150px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  border-radius: 20px;
+  margin-bottom: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  padding-left: 40px;
+`;
+
+const PostLink = styled(Link)`
+  text-decoration: none;
+`;
+
+const PostTitie = styled.div`
+  color: black;
+  font-size: 17px;
+  font-weight: 500;
+`;
+
+const PostContent = styled.div`
+  color: #828282;
+  font-size: 15px;
+  font-weight: 400;
+  margin-top: 20px;
+`;
 
 export default Home;
